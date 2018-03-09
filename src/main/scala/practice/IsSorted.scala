61484a23611bbb8f196a3777b3fc5757cabc5da4
SungJin-Jin <deskhi@nethru.co.kr>
2018-03-09 16:28:03 +0900

    Add isSorted example

---
+++ src/main/scala/practice/IsSorted.scala
@@ -0,0 +1,17 @@
+package practice
+
+import scala.annotation.tailrec
+
+object IsSorted {
+  def main(args: Array[String]) {
+
+    require(solve(Array(1, 2, 3, 4, 5), (a0: Int, a1: Int) => a0 <= a1))
+
+  }
+
+  @tailrec def solve[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    if (as.length <= 1) true
+    else if (!ordered(as(0), as(1))) false
+    else solve(as.drop(1), ordered)
+  }
+}
